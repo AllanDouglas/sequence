5a1196629b48bbcabd86a68d141be6f4e7c458e8
Allan Douglas <allandouglas@live.com>
2016-03-22 23:43:31 -0300

    primeiro commit

---
+++ Assets/Scripts/GridBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridBehaviourScript : MonoBehaviour {
+
+	private const float ESPACAMENTO_ENTRE_PECAS = 1.1f;
+
+	// tamanho do grid
+	[HideInInspector]
+	public int linhas, colunas;
+	// quantidade de movimentos para misturar 
+	[HideInInspector]
+	public int movimentos; 
+	// prefab para preenchimento do grid
+	[HideInInspector]
+	public GameObject prefabDoPreenchimento;
+
+
+	// array do grid deve
+	private GameObject[,] _grid;
+
+
+	// inicializar
+	public void Inicializar(){
+		this._grid = new GameObject[this.colunas,this.linhas];
+
+		for (int x = 0; x < colunas; x++) {
+			for (int y = 0; y < linhas; y++) {
+
+				_grid [x, y] = Instantiate (prefabDoPreenchimento); 
+				_grid [x, y].transform.position = new Vector2 (x * ESPACAMENTO_ENTRE_PECAS,y * ESPACAMENTO_ENTRE_PECAS);
+				_grid [x, y].transform.parent = transform;
+			}
+		}
+
+	}
+
+
+	// misturar
+
+
+}
---
+++ Assets/Scripts/LevelBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBehaviourScript : MonoBehaviour {
+
+	public int linhas;
+	public int colunas;
+
+	public GameObject prefab; 
+
+	public GridBehaviourScript Grid;
+
+	// Use this for initialization
+	void Start () {
+		Grid.colunas = colunas;
+		Grid.linhas = linhas;
+		Grid.prefabDoPreenchimento = prefab;
+
+		Grid.Inicializar ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
